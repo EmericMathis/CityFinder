4b884cfac48e0cad9004393ec8aa28bc08bdc4dd
EmericMathis <emericmathis@gmail.com>
2024-01-04 12:49:17 +0100

    css terminé

--- src/components/DisplayList/index.js
+++ src/components/DisplayList/index.js
@@ -1,13 +1,62 @@
 import React from 'react';
+import './index.scss';
 
 const DisplayList = () => {
     return (
         <div className='displaylist-container'>
             <ul>
                 <li className='city-container'>
-                    <div className="ville">ville</div>
-                    <div className="code-postal">code postal</div>
-                    <div className="population">population</div>
+                    <div className="ville">ville :
+                        <div className="numero"> 43</div>
+                    </div>
+                    <div className="code-postal">code postal :
+                        <div className="numero">43330</div>
+                    </div>
+                    <div className="population">population :
+                        <div className="numero"> 2341</div>
+                    </div>
+                </li>
+
+                <li className='city-container'>
+                    <div className="ville">ville :
+                        <div className="numero"> 43</div>
+                    </div>
+                    <div className="code-postal">code postal :
+                        <div className="numero">43330</div>
+                    </div>
+                    <div className="population">population :
+                        <div className="numero"> 2341</div>
+                    </div>
+                </li><li className='city-container'>
+                    <div className="ville">ville :
+                        <div className="numero"> 43</div>
+                    </div>
+                    <div className="code-postal">code postal :
+                        <div className="numero">43330</div>
+                    </div>
+                    <div className="population">population :
+                        <div className="numero"> 2341</div>
+                    </div>
+                </li><li className='city-container'>
+                    <div className="ville">ville :
+                        <div className="numero"> 43</div>
+                    </div>
+                    <div className="code-postal">code postal :
+                        <div className="numero">43330</div>
+                    </div>
+                    <div className="population">population :
+                        <div className="numero"> 2341</div>
+                    </div>
+                </li><li className='city-container'>
+                    <div className="ville">ville :
+                        <div className="numero"> 43</div>
+                    </div>
+                    <div className="code-postal">code postal :
+                        <div className="numero">43330</div>
+                    </div>
+                    <div className="population">population :
+                        <div className="numero"> 2341</div>
+                    </div>
                 </li>
             </ul>
         </div>
